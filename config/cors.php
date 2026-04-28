04a207cad9d2ccc0f6e402a22ce1a0cc6d4d26ce
JamshidGuliston <jamshidbotirov1992@gmail.com>
2026-04-28 15:06:14 +0500

    Update allowed_origins in CORS configuration to specify trusted domains

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths'                    => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods'          => ['*'],
-    'allowed_origins'          => ['*'],
+    'allowed_origins'          => ['https://sehrlikitoblar.uz', 'http://localhost:3000'],
     'allowed_origins_patterns' => [],
     'allowed_headers'          => ['*'],
     'exposed_headers'          => ['X-Request-ID'],
